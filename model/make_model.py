1e6a34a31f6d9e510883a01a7494dad294fa81ee
hjm8377 <hjm8377@gmail.com>
2025-08-22 17:13:12 +0900

    making model...

--- model/make_model.py
+++ model/make_model.py
@@ -1,3 +1,114 @@
 import torch
 import torch.nn as nn
+from .clip import clip
 
+def weights_init_kaiming(m):
+    classname = m.__class__.__name__
+    if classname.find('Linear') != -1:
+        nn.init.kaiming_normal_(m.weight, a=0, mode='fan_out')
+        nn.init.constant_(m.bias, 0.0)
+
+    elif classname.find('Conv') != -1:
+        nn.init.kaiming_normal_(m.weight, a=0, mode='fan_in')
+        if m.bias is not None:
+            nn.init.constant_(m.bias, 0.0)
+    elif classname.find('BatchNorm') != -1:
+        if m.affine:
+            nn.init.constant_(m.weight, 1.0)
+            nn.init.constant_(m.bias, 0.0)
+
+def weights_init_classifier(m):
+    classname = m.__class__.__name__
+    if classname.find('Linear') != -1:
+        nn.init.normal_(m.weight, std=0.001)
+        if m.bias:
+            nn.init.constant_(m.bias, 0.0)
+
+def load_clip_to_cpu(backbone_name, h_resolution, w_resolution, vision_stride_size):
+        url = clip._MODELS[backbone_name]
+        model_path = clip._download(url)
+
+        try:
+            # loading JIT archive
+            model = torch.jit.load(model_path, map_location="cpu").eval()
+            state_dict = None
+
+        except RuntimeError:
+            state_dict = torch.load(model_path, map_location="cpu")
+
+        model = clip.build_model(state_dict or model.state_dict(), h_resolution, w_resolution, vision_stride_size)
+
+        return model
+
+
+class TextEncoder(nn.Module):
+    def __init__(self, clip_model):
+        super().__init__()
+        self.transformer = clip_model.transformer
+        self.positional_embedding = clip_model.positional_embedding
+        self.ln_final = clip_model.ln_final
+        self.text_projection = clip_model.text_projection
+        self.dtype = clip_model.dtype
+
+    def forward(self, prompts, tokenized_prompts): 
+        x = prompts + self.positional_embedding.type(self.dtype) 
+        x = x.permute(1, 0, 2)  # NLD -> LND 
+        x = self.transformer(x) 
+        x = x.permute(1, 0, 2)  # LND -> NLD
+        x = self.ln_final(x).type(self.dtype) 
+
+        # x.shape = [batch_size, n_ctx, transformer.width]
+        # take features from the eot embedding (eot_token is the highest number in each sequence)
+        x = x[torch.arange(x.shape[0]), tokenized_prompts.argmax(dim=-1)] @ self.text_projection 
+        return x
+
+
+class build_transformer(nn.Module):
+    def __init__(self, num_classes, camera_num, view_num, cfg):
+        super(build_transformer, self).__init__()
+        self.model_name = cfg.MODEL.NAME
+
+        self.in_planes = 768
+        self.in_planes_proj = 512
+
+        self.num_classes = num_classes
+        self.camera_num = camera_num
+        self.view_num = view_num
+
+        self.classifier = nn.Linear(self.in_planes, self.num_classes, bias=False)
+        self.classifier.apply(weights_init_classifier)
+        self.classifier_proj = nn.Linear(self.in_planes_proj, self.num_classes, bias=False)
+        self.classifier_proj.apply(weights_init_classifier)
+
+        self.bottleneck = nn.BatchNorm1d(self.in_planes)
+        self.bottleneck.bias.requires_grad_(False)
+        self.bottleneck.apply(weights_init_kaiming)
+        self.bottleneck_proj = nn.BatchNorm1d(self.in_planes_proj)
+        self.bottleneck_proj.bias.requires_grad_(False)
+        self.bottleneck_proj.apply(weights_init_kaiming)
+
+        self.h_resolution = int((cfg.INPUT.SIZE_TRAIN[0]-16)//cfg.MODEL.STRIDE_SIZE[0] + 1)
+        self.w_resolution = int((cfg.INPUT.SIZE_TRAIN[1]-16)//cfg.MODEL.STRIDE_SIZE[1] + 1)
+        self.vision_stride_size = cfg.MODEL.STRIDE_SIZE[0]
+        clip_model = load_clip_to_cpu(self.model_name, self.h_resolution, self.w_resolution, self.vision_stride_size)
+        clip_model.to("cuda")
+
+        self.image_encoder = clip_model.visual
+
+        dataset_name = cfg.DATASETS.NAMES
+        self.text_encoder = TextEncoder(clip_model)
+
+        # freeze text encoder
+        for p in self.text_encoder.parameters():
+            p.requires_grad = False
+
+        for p in self.token_embedding.parameters():
+            p.requires_grad = False
+
+
+    def forward(self, x, label, get_image, get_text, cam_label, view_label):
+        pass
+
+def make_model(cfg, num_class, camera_view, view_num):
+    model = build_transformer(num_class, camera_view, view_num, cfg)
+    return model
